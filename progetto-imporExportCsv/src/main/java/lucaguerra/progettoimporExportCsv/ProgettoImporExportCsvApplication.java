a70c0fe20d52cf0eec6b8dc454905c18606914e1
lucawar <lucaguerra92@yahoo.it>
2024-02-17 19:24:44 +0100

    inizializzazione

---
+++ progetto-imporExportCsv/src/main/java/lucaguerra/progettoimporExportCsv/ProgettoImporExportCsvApplication.java
@@ -0,0 +1,13 @@
+package lucaguerra.progettoimporExportCsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoImporExportCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoImporExportCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucaguerra/progettoimporExportCsv/ProgettoImporExportCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package lucaguerra.progettoimporExportCsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoImporExportCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
